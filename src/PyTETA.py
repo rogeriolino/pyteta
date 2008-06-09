ca5733dece358af247030c473de558cb7156a738
rogeriolino@gmail.com <rogeriolino@gmail.com@bb17b7a0-f04e-0410-8bbe-d551c080da7a>
2008-06-09 17:40:14 +0000

    Adicionado comentários nos arquivos PyTETA.py e Server.py

--- src/PyTETA.py
+++ src/PyTETA.py
@@ -1,5 +1,17 @@
 # -*- coding: UTF-8 -*-
 
+"""
+
+    PyTETA Client
+    
+    Interface grafica para utilizar o sistema de chat, modo cliente, baseado no protocolo
+    P.A.T.E.T.A. (Protocolo Aberto de Transferência Especialmente para Trabalhos Acadêmicos)
+    
+    @author: Rogério Alencar Lino Filho
+    @version: 0.0.1
+
+"""
+
 
 if __name__ == '__main__':
 
--- src/Server.py
+++ src/Server.py
@@ -1,5 +1,17 @@
 # -*- coding: UTF-8 -*-
 
+"""
+
+    PyTETA Server
+    
+    Interface grafica para utilizar o sistema de chat, modo servidor, baseado no protocolo
+    P.A.T.E.T.A. (Protocolo Aberto de Transferência Especialmente para Trabalhos Acadêmicos)
+    
+    @author: Rogério Alencar Lino Filho
+    @version: 0.0.1
+
+"""
+
 from lib.server import Server
 
 if __name__ == '__main__':
