63c6da8e21c946787355a482b8b1e1a34dda6098
rogeriolino@gmail.com <rogeriolino@gmail.com@bb17b7a0-f04e-0410-8bbe-d551c080da7a>
2008-06-17 18:51:13 +0000

    Alterada versão o PyTETA.

--- src/PyTETA.py
+++ src/PyTETA.py
@@ -8,7 +8,7 @@
     P.A.T.E.T.A. (Protocolo Aberto de Transferência Especialmente para Trabalhos Acadêmicos)
     
     @author: Rogério Alencar Lino Filho
-    @version: 0.0.1
+    @version: 1.0.0
 
 """
 
